5ae84963f4466d57ffb3247ae98b30289f1c3851
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-06-05 10:51:30 +0300

    update onn teh dashboard

--- web_flask/static/js/company/company_data.js
+++ web_flask/static/js/company/company_data.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
 
 $(function() {
-    var tbody = $("#customer-list")
-            tbody.empty()
+    var data_table = $("#example")
+    data_table.find("tbody").empty()    
     let row = $("<tr>")
             row.append($("<th>").text("#2457"))
             row.append($("<td>").text("Brandon Jacob"))
             row.append($("<td>").text("At praesentium minu"))
             row.append($("<td>").text("$64"))
             row.append($("<td>").text("Approved"))
-        tbody.append(row)
+        data_table.find("tbody").append(row)
         console.log("appended done")
+
+        $('#example').DataTable({
+                destroy: true, // Destroy existing DataTable before creating a new one
+                paging: true,
+                
+                buttons: [
+                    'csv', 'excel', 'pdf', 'print'
+                ],
+                responsive: true
+            });
 })
--- web_flask/static/js/company/dashboard.js
+++ web_flask/static/js/company/dashboard.js
@@ -6,6 +6,8 @@ import { getLS } from "../cookies.js";
 import { formatDate } from "../date_format.js"
 
 $(function() {
+    var data_table = $("#example")
+    data_table.find("tbody").empty()
     $.when(
         ajax_request(apiUrl + `order`, "GET", getLS("company")),
         ajax_request(apiUrl + `customer`, "GET", getLS("company")),
@@ -13,5 +15,35 @@ $(function() {
         ajax_request(apiUrl + "inventory", "GET", getLS("company"))
     ).then((orders, customers, orderitems, products) => {
         
+        orders.forEach(order =>{
+            orderitems.forEach(orderitem => {
+                if (order.id === orderitem.order_id) {
+                    let customer = customers.find(cust_data => cust_data.id === order.cus_id)
+                    let product = products.find(item => item.id === orderitem.prod_id)
+                    let row = $("<tr>")
+                    row.append($("<th>").text(order.txn_no))
+                    row.append($("<td>").text(customer.full_name))
+                    row.append($("<td>").text(product.product))
+                    row.append($("<td>").text(product.price))
+                    row.append($("<td>").text(order.status))
+                    data_table.find("tbody").append(row)
+                }
+            })
+                    
+        })
+    
+
+        $('#example').DataTable({
+                destroy: true, // Destroy existing DataTable before creating a new one
+                paging: true,
+                searchable: true,
+                buttons: [
+                    'csv', 'excel', 'pdf', 'print'
+                ],
+                responsive: true
+            });
+
     })
+
+    
 })
